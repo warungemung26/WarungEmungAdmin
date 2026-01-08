1c736161b8137194a10a9c1aa0e40783142c0bf3
warungemung26 <warung.emung26@gmail.com>
2026-01-08 15:13:21 +0700

    push zip

---
+++ js/wa-config.js
@@ -0,0 +1,35 @@
+/*!
+ * Copyright (c) 2025, Atos
+ * All rights reserved.
+ * Unauthorized copying, modification, or distribution of this file is strictly prohibited.
+ */
+
+window.WA_CONFIG = {
+  number: "6285220370383",
+
+  templates: {
+    request:
+`Halo Warung Emung,
+
+Saya ingin request produk:
+
+ Produk: {{produk}}
+ Jumlah: {{jumlah}}
+
+Terima kasih.`,
+
+    cek:
+`Halo Warung Emung,
+Saya ingin menanyakan status pesanan terbaru saya.
+
+ ID Pesanan: {{id}}
+ Nama: {{nama}}
+ Alamat: {{alamat}}
+ HP: {{hp}}
+ Produk: {{produk}}
+ Total: {{total}}
+ Waktu: {{waktu}}
+
+Mohon informasinya.`
+  }
+};
